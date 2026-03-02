192477995045c08fb1b19f3b869a99d1cb2b9ce2
Laila <hatibulaila@gmail.com>
2026-03-02 19:58:08 +0300

    add something

--- Backened projects/Onlinebookingproject/Onlinebookingproject/settings.py
+++ Backened projects/Onlinebookingproject/Onlinebookingproject/settings.py
@@ -140,5 +140,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+CORS_ALLOWED_ORIGINS = [
+    'https://online-booking-ticket.vercel.app'
+]
+CORS_ALLOWED_ORIGIN_ALL = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
